bd4731409fa22419aacfea6e106e9ca1e8e2418d
evanpalsson <evanpalsson@gmail.com>
2015-03-03 14:07:00 -0700

    evan is so cool

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -2,7 +2,15 @@ var app = angular.module("pollApp");
 
 app.controller("mainCtrl", function($scope, mainService) {
 
+	var socket = io.connect();
 
+		$scope.Opt1 = options;
+		console.log('hello');
+
+		$scope.createPoll = function(options){
+			socket.emit('poll', options);
+			console.log(options);		
+		};
 
 
 })
