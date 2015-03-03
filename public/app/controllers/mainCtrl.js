6fb52bde29bded814c8ece313c3ccc3bc5d2f24e
Michael Telford <mgtelford@gmail.com>
2015-03-03 16:45:16 -0700

    Team Update

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -2,15 +2,22 @@ var app = angular.module("pollApp");
 
 app.controller("mainCtrl", function($scope, mainService) {
 
-	var socket = io.connect();
 
-		$scope.Opt1 = options;
-		console.log('hello');
 
-		$scope.createPoll = function(options){
-			socket.emit('poll', options);
-			console.log(options);		
+	//var socket = io.connect();
+    //
+	//	$scope.Opt1 = options;
+	//	console.log('hello');
+
+		$scope.createPoll = function(){
+            var newPoll = [$scope.choice1, $scope.choice2, $scope.choice3, $scope.choice4];
+
+                mainService.createPoll(newPoll);
+
+
+			//socket.emit('poll', options);
+			//console.log(options);
 		};
 
 
-})
+});
