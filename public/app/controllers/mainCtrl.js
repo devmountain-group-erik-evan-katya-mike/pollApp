cd57e33a46451ec258fda620b86ae9cfdd31faba
erikanderson <edawebdesign@gmail.com>
2015-03-04 16:23:06 -0700

    move files

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -23,14 +23,4 @@ app.controller("mainCtrl", function($scope, mainService, authService) {
       })
   }
 
-
-  $scope.createPoll = function(){
-    var newTitle = $scope.pollTitle;
-    var newPoll = [{"body":$scope.choice1}, {"body":$scope.choice2}, {"body":$scope.choice3}, {"body":$scope.choice4}];
-
-    mainService.createPoll(newTitle, newPoll);
-    //socket.emit('poll', options);
-		//console.log(options);
-	};
-
 });
